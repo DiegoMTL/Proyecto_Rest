5da1322b309940ae3f2d435924b60c592a4fe4f4
Jennifer <jennifer.portinof@utem.cl>
2021-07-10 20:17:32 -0400

    Add: Orden de los datos obtenidos en Scraping

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -2,7 +2,8 @@ const { Pool } = require('pg');//manera para conectarnos a postgres
 const cheerio = require('cheerio');
 const request = require('request-promise');
 
-async function data(){
+async function scraping(){
+    const data= [];
     const $ = await request({ //aqui tengo todo el documento
         uri: 'http://www.sismologia.cl/links/ultimos_sismos.html',
         transform: body => cheerio.load(body)
@@ -15,9 +16,22 @@ async function data(){
             const pro = $(el).find('td').next().next().next().next();
             const mag = $(el).find('td').next().next().next().next().next();
             const ref = $(el).find('td').next().next().next().next().next().next().next();
-            console.log(i,f, la.html(), lon.html(), pro.html(), mag.html(), ref.html());
+            //console.log(i,f, la.html(), lon.html(), pro.html(), mag.html(), ref.html()); 
+            const sismo = {
+                id: i,
+                fecha: f, 
+                latitud: parseFloat(la.html()), //double
+                longitud: parseFloat(lon.html()), //double
+                profundidad: parseInt(pro.html()), //int
+                magnitud:  parseFloat(mag.html()), //double
+                referencia: ref.html()
+            };
+            data[i-1] = sismo;  
         }
     });
+    //console.log(data);
+    console.log("Scraping");
+    return data; 
 }
 
 const pool  = new Pool ({
@@ -30,12 +44,13 @@ const pool  = new Pool ({
 });
 
 const getTerremoto = async (req, res) => {
+    const sismos = [];
     /*const response = await pool.query('SELECT * FROM terremoto'); //consulta a la base de datos terremotos 
     console.log(response.rows); //impresion por consola
     res.status(200).json(response.rows); //impresion navegador para el estado 200
     console.log(req.body);
     res.send('Get Terremotos');*/
-    data();
+    sismos = scraping();
 };
 
 const createTerremoto = async (req,res)=>{
